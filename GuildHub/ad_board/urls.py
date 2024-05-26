972d16220d0428406af088f90f655e171ddeb4b0
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-05-27 01:37:04 +0300

    git commit -m "Added ad management buttons and unpublished status display
    - Added 'Publish', 'Unpublish', and 'Delete' buttons for ad management on the ad detail page.
    - Displayed red 'unpublished' text in the user badge for unpublished ads."

--- GuildHub/ad_board/urls.py
+++ GuildHub/ad_board/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from ad_board.views import AdBoardView, AdDetailView, AdCreateView
+from ad_board.views import AdBoardView, AdDetailView, AdCreateView, AdUpdateView, ad_publish, ad_unpublish, ad_delete
 
 urlpatterns = [
     path('', AdBoardView.as_view(), name='ad_board'),
     path('<int:pk>/', AdDetailView.as_view(), name='ad_detail'),
     path('create/', AdCreateView.as_view(), name='ad_create'),
+    path('<int:pk>/update/', AdUpdateView.as_view(), name='ad_update'),
+    path('<int:pk>/publish/', ad_publish, name='ad_publish'),
+    path('<int:pk>/unpublish/', ad_unpublish, name='ad_unpublish'),
+    path('<int:pk>/delete/', ad_delete, name='ad_delete'),
+
 ]
--- GuildHub/ad_board/views.py
+++ GuildHub/ad_board/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.utils.translation import gettext as _
+from django.contrib import messages
 
 from .filters import AdFilter
 from .forms import AdForm
@@ -27,16 +32,6 @@ class AdBoardView(ListView):
         return context
 
 
-class AdCreateView(LoginRequiredMixin, CreateView):
-    form_class = AdForm
-    model = Ad
-    template_name = 'ad_board/ad_create.html'
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-
 class AdDetailView(DetailView):
     model = Ad
     template_name = 'ad_board/ad_detail.html'
@@ -52,3 +47,76 @@ class AdDetailView(DetailView):
         if not obj.can_view(self.request.user):
             raise PermissionDenied
         return obj
+
+
+class AdCreateView(LoginRequiredMixin, CreateView):
+    form_class = AdForm
+    model = Ad
+    template_name = 'ad_board/ad_create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = False
+        return context
+
+
+class AdUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = AdForm
+    model = Ad
+    template_name = 'ad_board/ad_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = True
+        return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from .models import Ad
+
+@login_required
+def ad_delete(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    if request.user == ad.user or request.user.is_staff:
+        ad.delete()
+        messages.success(request, _('Ad has been deleted successfully.'))
+        return redirect('ad_board')
+    else:
+        messages.error(request, _('You do not have permission to delete this ad.'))
+        return redirect('ad_detail', pk=pk)
+
+
+
+@login_required
+@require_POST
+def ad_publish(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    if request.user == ad.user or request.user.is_staff:
+        ad.publish_ad()
+        messages.success(request, _('Ad has been published successfully.'))
+    else:
+        messages.error(request, _('You do not have permission to publish this ad.'))
+    return redirect('ad_detail', pk=pk)
+
+
+@login_required
+@require_POST
+def ad_unpublish(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    if request.user == ad.user or request.user.is_staff:
+        ad.unpublish_ad()
+        messages.success(request, _('Ad has been unpublished successfully.'))
+    else:
+        messages.error(request, _('You do not have permission to unpublish this ad.'))
+    return redirect('ad_detail', pk=pk)
