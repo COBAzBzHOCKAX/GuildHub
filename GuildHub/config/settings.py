6e015952f0385a78c7f7f1c56a5528350773347b
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-06-29 12:45:47 +0300

    fix STATIC_ROOT

--- GuildHub/config/settings.py
+++ GuildHub/config/settings.py
@@ -222,15 +222,11 @@ LOCALE_PATHS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static/"
 
-# Media files (uploaded user content)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media/"
 
 LOGGING = {
     'version': 1,
