28cf25ea6c345d00ba6f8bb9a64770c2a0c73b29
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-07-01 04:21:09 +0300

    add static media

--- GuildHub/config/settings.py
+++ GuildHub/config/settings.py
@@ -36,6 +36,12 @@ CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = ["127.0.0.1", "COBAzBzHOCKAX.pythonanywhere.com"]
 
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Application definition
 INSTALLED_APPS = [
     # localization
@@ -219,14 +225,6 @@ USE_TZ = True
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale')
 ]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGGING = {
--- GuildHub/users/urls.py
+++ GuildHub/users/urls.py
@@ -1,5 +1,7 @@
+from django.template.context_processors import static
 from django.urls import path
 
+from config import settings
 from users.views import UserDetailView, UserUpdateView, UserDeactivateView
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     path('<int:pk>/update/', UserUpdateView.as_view(), name='profile_update'),
     path('<int:pk>/deactivate/', UserDeactivateView.as_view(), name='profile_deactivate_url')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
