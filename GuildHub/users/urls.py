bd48e4a394a087c7dd642524f83169571f9a99ca
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-07-01 04:33:37 +0300

    fix static media

--- GuildHub/users/urls.py
+++ GuildHub/users/urls.py
@@ -1,7 +1,5 @@
-from django.template.context_processors import static
 from django.urls import path
 
-from config import settings
 from users.views import UserDetailView, UserUpdateView, UserDeactivateView
 
 urlpatterns = [
@@ -9,6 +7,3 @@ urlpatterns = [
     path('<int:pk>/update/', UserUpdateView.as_view(), name='profile_update'),
     path('<int:pk>/deactivate/', UserDeactivateView.as_view(), name='profile_deactivate_url')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
